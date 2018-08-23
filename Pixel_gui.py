a48190a6e47e758e89bbfdd05c0e3190879a0e97
cminmins <41716029+cminmins@users.noreply.github.com>
2018-08-23 22:33:35 +0900

    Crop mr / 비율변경완료

--- Pixel_gui.py
+++ Pixel_gui.py
@@ -2,11 +2,12 @@ import sys, os, glob
 import pydicom
 import numpy as np
 import skimage.io as sk
+from skimage.transform import resize
+
 from skimage import data, io, filters, color
 
 from PyQt5 import QtCore, uic, QtWidgets, QtGui
 gui = uic.loadUiType("untitled.ui")[0]
-
 
 class Pixel_gui(QtWidgets.QMainWindow, gui):
 
@@ -15,13 +16,20 @@ class Pixel_gui(QtWidgets.QMainWindow, gui):
         self.setupUi(self)
         self.dcmfilect = ''
         self.dcmfilemr = ''
-        self.ct_pix = ''
-        self.mr_pix = ''
+        self.resizemr = ''
+        self.save_ct = ''
+        self.save_mr = ''
+        self.mr_pos_x = ''
+        self.mr_pos_y = ''
+        self.name_count = 1
 
         ## 디렉토리 열고 dcm 리스트까지 추가    
         dir_name = ''
         self.Button_open.clicked.connect(self.opendirectory)
         self.List_directory.itemClicked.connect(self.choosedirectory)
+
+        ##save 버튼
+        self.Button_save.clicked.connect(self.savetiff)
 
         ## dcm 파일 클릭하면 View에 이미지 띄우기
         self.List_CT.itemClicked.connect(self.selectimageCT)
@@ -31,38 +39,102 @@ class Pixel_gui(QtWidgets.QMainWindow, gui):
         self.qlabel_MR.mousePressEvent = self.cropMR
         self.qlabel_overay.mousePressEvent = self.overlay
 
-    def cropCT(self, event):
-        x = event.pos().x()
-        y = event.pos().y()
-        CTcrop_x = int(x * 4)
-        CTcrop_y = int(y * 4)
+        self.mr_hor.valueChanged.connect(self.changeRatio)
+        self.mr_ver.valueChanged.connect(self.changeRatio)
 
-        CTcrop_image = np.copy(self.dcmfilect.pixel_array[CTcrop_y:CTcrop_y+800, CTcrop_x:CTcrop_x+700])
-        CTcrop_image = (CTcrop_image / 16).astype(np.uint8)
-        CTcrop_image = np.require(CTcrop_image, np.uint8, 'C')
-        h, w = CTcrop_image.shape
-        result = QtGui.QImage(CTcrop_image.data, w, h, QtGui.QImage.Format_Grayscale8)
-        pixmap = QtGui.QPixmap(result)
-        self.ct_pix = pixmap
-        self.qlabel_CT_crop.setPixmap(pixmap)
+    def overlay(self, event):
+        CTover_image = (self.save_ct / 16).astype(np.uint8)
+        CTover_image = np.require(CTover_image, np.uint8, 'C')
+        h, w = CTover_image.shape
+        ct_result = QtGui.QImage(CTover_image.data, w, h, QtGui.QImage.Format_Grayscale8)
+        ct_pixmap = QtGui.QPixmap(ct_result)
 
-    def cropMR(self, event):
-        x = event.pos().x()
-        y = event.pos().y()
+        MRover_image = (self.save_mr / 16).astype(np.uint8)
+        MRover_image = np.require(MRover_image, np.uint8, 'C')
+        h, w = MRover_image.shape
+        mr_result = QtGui.QImage(MRover_image.data, w, h, QtGui.QImage.Format_Grayscale8)
+        mr_pixmap = QtGui.QPixmap(mr_result)
 
-        MRcrop_image = np.copy(self.dcmfilemr.pixel_array[y:y+200, x:x+180])
+
+        painter = QtGui.QPainter()
+        painter.begin(ct_result)
+        painter.drawImage(0, 0, mr_result)
+        painter.end()
+
+        self.qlabel_overay.setPixmap(QtGui.QPixmap.fromImage(ct_result))
+
+
+
+    def savetiff(self):
+        ct_name = str(self.name_count) + "_ct.tif"
+        mr_name = str(self.name_count) + "_mr.tif"
+        sk.imsave(ct_name, self.save_ct, plugin='tifffile')
+        sk.imsave(mr_name, self.save_mr, plugin='tifffile')
+        self.name_count = self.name_count + 1
+
+    def changeRatio(self):
+        x = self.mr_pos_x
+        y = self.mr_pos_y
+        change_width = 700 + self.mr_hor.value()
+        change_height = 800 + self.mr_ver.value()
+
+        MRcrop_image = np.copy(self.resizemr[y:y+change_height, x:x+change_width])
+        self.save_mr = resize(MRcrop_image, (800, 700), anti_aliasing=True, order=3)
+        self.save_mr = (self.save_mr * 65535).astype(np.uint16)
+        MRcrop_image = self.save_mr
+
+        ## 이미지를 화면에 띄위기 위해서 8비트로 낮춤
         MRcrop_image = (MRcrop_image / 16).astype(np.uint8)
         MRcrop_image = np.require(MRcrop_image, np.uint8, 'C')
         h, w = MRcrop_image.shape
         result = QtGui.QImage(MRcrop_image.data, w, h, QtGui.QImage.Format_Grayscale8)
         pixmap = QtGui.QPixmap(result)
-        pixmap = pixmap.scaled(self.qlabel_MR_crop.width(), self.qlabel_MR_crop.height(), QtCore.Qt.KeepAspectRatio)
-        self.mr_pix = pixmap
         self.qlabel_MR_crop.setPixmap(pixmap)
 
-    def overlay(self, event):
-        over_pix = self.mr_pix - self.ct_pix + 1024
-        self.qlabel_overay.setPixmap(over_pix)
+    def cropCT(self, event):
+        ## 마우스 클릭한 곳의 좌표 256 -> 1024
+        x = event.pos().x()
+        y = event.pos().y()
+        CTcrop_x = int(x * 4)
+        CTcrop_y = int(y * 4)
+
+        ## 원본 이미지 배열을 복사하고 저장할 변수에도 예비 저장
+        CTcrop_image = np.copy(self.dcmfilect.pixel_array[CTcrop_y:CTcrop_y+800, CTcrop_x:CTcrop_x+700])
+        self.save_ct = CTcrop_image.astype(np.uint16)
+        print((CTcrop_image == self.save_ct).all())
+
+        ## 이미지를 화면에 띄우기 위해서 8비트로 낮춤
+        CTcrop_image = (CTcrop_image / 16).astype(np.uint8)
+        CTcrop_image = np.require(CTcrop_image, np.uint8, 'C')
+        h, w = CTcrop_image.shape
+        result = QtGui.QImage(CTcrop_image.data, w, h, QtGui.QImage.Format_Grayscale8)
+        pixmap = QtGui.QPixmap(result)
+        self.qlabel_CT_crop.setPixmap(pixmap)
+
+    def cropMR(self, event):
+        ## 마우스 클릭한 곳의 좌표 256 -> 256
+        self.mr_pos_x = event.pos().x() * 4
+        self.mr_pos_y = event.pos().y() * 4
+        x = self.mr_pos_x
+        y = self.mr_pos_y
+
+        ## 원본 256 -> 1024로 변환 후 Crop해서 저장변수에 저장
+        MRcrop_image = np.copy(self.resizemr)
+        self.save_mr = np.copy(MRcrop_image[y:y+800, x:x+700])
+
+        # self.save_mr = resize(MRcrop_image, (800, 700), anti_aliasing=True, order=3)
+        # self.save_mr = (self.save_mr * 32767).astype(np.uint16)
+        # self.save_mr = self.save_mr.astype(np.uint16)
+        MRcrop_image = self.save_mr
+
+        ## 이미지를 화면에 띄위기 위해서 8비트로 낮춤
+        MRcrop_image = (MRcrop_image / 16).astype(np.uint8)
+        MRcrop_image = np.require(MRcrop_image, np.uint8, 'C')
+        h, w = MRcrop_image.shape
+        result = QtGui.QImage(MRcrop_image.data, w, h, QtGui.QImage.Format_Grayscale8)
+        pixmap = QtGui.QPixmap(result)
+        # pixmap = pixmap.scaled(self.qlabel_MR_crop.width(), self.qlabel_MR_crop.height(), QtCore.Qt.KeepAspectRatio)
+        self.qlabel_MR_crop.setPixmap(pixmap)
 
     def selectimageCT(self):
         dcmitem = self.List_CT.selectedItems()[0].text()
@@ -81,6 +153,11 @@ class Pixel_gui(QtWidgets.QMainWindow, gui):
     def selectimageMR(self):
         dcmitem = self.List_MR.selectedItems()[0].text()
         self.dcmfilemr = pydicom.dcmread(dcmitem)
+
+        self.resizemr = np.copy(self.dcmfilemr.pixel_array)
+        self.resizemr = resize(self.resizemr, (1024, 1024), anti_aliasing=True, order=3)
+        self.resizemr = (self.resizemr * 32767).astype(np.uint16)
+
         MRimage = np.copy(self.dcmfilemr.pixel_array)
 
         MRimage = (MRimage / 16).astype(np.uint8)
@@ -106,6 +183,7 @@ class Pixel_gui(QtWidgets.QMainWindow, gui):
         ## 아이템 선택 할때 먼저 초기화
         self.List_CT.clear()
         self.List_MR.clear()
+        self.name_count = 1
 
         ## 선택한 아이템 텍스트 저장
         selecteditem = self.List_directory.selectedItems()[0].text()
@@ -142,4 +220,9 @@ if __name__ == '__main__':
 # scene = QtWidgets.QGraphicsScene(self)
 # scene.addPixmap(pixmap)
 # self.graphics_CT.setScene(scene)
+
+
+
+# medcon -f *.nii -split3d -c dicom -b16
+# medcon -f *.dcm -stack3d -c nifty -b16
 
--- pixeltest.py
+++ pixeltest.py
@@ -3,58 +3,62 @@ import pydicom
 from dipy.align.reslice import reslice
 import cv2
 import skimage.io as skimage
+from skimage.transform import resize
 from skimage import data, io, filters, color
 import numpy as np
-
-org_ct = pydicom.dcmread('30005.dcm')
-a1 = org_ct.pixel_array
-after_org = pydicom.dcmread('m000-s0001-Reslice_ct.dcm')
-a2 = after_org.pixel_array
-
-org_mr = pydicom.dcmread('60002.dcm')
-b1 = org_mr.pixel_array
-res_dcm = pydicom.dcmread('m001-s0001-Reslice_mri.dcm')
-b2 = res_dcm.pixel_array
-
-print((a1==a2).all())
-print((b1==b2).all())
-
-CT_image = nib.load('m000-stacks-30005.nii')
-CT_array = np.copy(CT_image.get_data())
-CT_array = CT_array.astype(np.int16)
-CT_affine = CT_image.affine
-CT_zooms = CT_image.header.get_zooms()
-
-### MRI read
-MRT2_image = nib.load('m000-stacks-60002.nii')
-MRT2_array = np.copy(MRT2_image.get_data())
-MRT2_array = MRT2_array.astype(np.int16)
-MRT2_affine = MRT2_image.affine
-MRT2_zooms = MRT2_image.header.get_zooms()
-
-### CT의 zoom 수정
-# CT_zooma = CT_image.header['dim'][1] / MRT2_image.header['dim'][1] * CT_zooms[0]
-# CT_zoomb = CT_image.header['dim'][2] / MRT2_image.header['dim'][2] * CT_zooms[1]
-CT_zoomc = CT_image.header['dim'][3] / MRT2_image.header['dim'][3] * CT_zooms[2]
-
-### CT의 바뀐 zoom 적용
-# change_zoomCT_L = [CT_zooma, CT_zoomb, CT_zoomc]
-change_zoomCT_L = [CT_zooms[0], CT_zooms[1], CT_zoomc]
-change_zoomCT = tuple(change_zoomCT_L)
-
-### 수정된 zoom으로 Reslice
-newCT_brain, newCT_affine = reslice(CT_array, CT_affine, CT_zooms, CT_zooms)
-newT2_brain, newT2_affine = reslice(MRT2_array, MRT2_affine, MRT2_zooms, MRT2_zooms)
-
-### Reslice한 파일 저장
-newCT_image = nib.Nifti1Image(newCT_brain, newCT_affine)
-newT2_image = nib.Nifti1Image(newT2_brain, newT2_affine)
-nib.save(newCT_image, "Reslice_ct.nii")
-nib.save(newT2_image, "Reslice_mri.nii")
+#
+# org_ct = pydicom.dcmread('30005.dcm')
+# a1 = org_ct.pixel_array
+# after_org = pydicom.dcmread('m000-s0001-Reslice_ct.dcm')
+# a2 = after_org.pixel_array
+#
+# org_mr = pydicom.dcmread('60002.dcm')
+# b1 = org_mr.pixel_array
+# res_dcm = pydicom.dcmread('m001-s0001-Reslice_mri.dcm')
+# b2 = res_dcm.pixel_array
+#
+# print((a1==a2).all())
+# print((b1==b2).all())
+#
+# CT_image = nib.load('m000-stacks-30005.nii')
+# CT_array = np.copy(CT_image.get_data())
+# CT_array = CT_array.astype(np.int16)
+# CT_affine = CT_image.affine
+# CT_zooms = CT_image.header.get_zooms()
+#
+# ### MRI read
+# MRT2_image = nib.load('m000-stacks-60002.nii')
+# MRT2_array = np.copy(MRT2_image.get_data())
+# MRT2_array = MRT2_array.astype(np.int16)
+# MRT2_affine = MRT2_image.affine
+# MRT2_zooms = MRT2_image.header.get_zooms()
+#
+# ### CT의 zoom 수정
+# # CT_zooma = CT_image.header['dim'][1] / MRT2_image.header['dim'][1] * CT_zooms[0]
+# # CT_zoomb = CT_image.header['dim'][2] / MRT2_image.header['dim'][2] * CT_zooms[1]
+# CT_zoomc = CT_image.header['dim'][3] / MRT2_image.header['dim'][3] * CT_zooms[2]
+#
+# ### CT의 바뀐 zoom 적용
+# # change_zoomCT_L = [CT_zooma, CT_zoomb, CT_zoomc]
+# change_zoomCT_L = [CT_zooms[0], CT_zooms[1], CT_zoomc]
+# change_zoomCT = tuple(change_zoomCT_L)
+#
+# ### 수정된 zoom으로 Reslice
+# newCT_brain, newCT_affine = reslice(CT_array, CT_affine, CT_zooms, CT_zooms)
+# newT2_brain, newT2_affine = reslice(MRT2_array, MRT2_affine, MRT2_zooms, MRT2_zooms)
+#
+# ### Reslice한 파일 저장
+# newCT_image = nib.Nifti1Image(newCT_brain, newCT_affine)
+# newT2_image = nib.Nifti1Image(newT2_brain, newT2_affine)
+# nib.save(newCT_image, "Reslice_ct.nii")
+# nib.save(newT2_image, "Reslice_mri.nii")
 
 
 # # skimage.imsave('test.tif', program.pixel_array, plugin="tifffile")
-# ski_prog = skimage.imread('test.tif')
+ski_prog = skimage.imread('test.tif')
+test = resize(ski_prog, (800, 800), anti_aliasing=False)
+io.imshow(test)
+io.show()
 # ski_prog_gray = skimage.imread('test.tif', as_gray=True)
 
 #### skimage로 저장하면 무압축
